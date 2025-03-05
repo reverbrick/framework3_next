aef04c425050a9a42df53ecf8e550567f9222cdc
Daniel Górny <reverbrick@interia.pl>
2025-03-05 12:54:41 +0100

    dynamic form

--- src/components/dynamic-form.tsx
+++ src/components/dynamic-form.tsx
@@ -9,7 +9,7 @@ import type {
   UISchemaGroupItem,
   UISchemaNamedGroupItem,
   UISchemaItem,
-} from "@/app/lib/definitions";
+} from "@/lib/definitions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
